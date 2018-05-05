a40b840f4a45827223a28ad09dd3b8c2651b6c3b
Jonathan Bouchard <jonathaneutron@hotmail.com>
2018-05-05 04:11:28 -0400

    Beginning Map

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
 import Drawer from 'react-drag-drawer';
-import _ from 'lodash';
+import { Button } from 'antd';
+import MapComponent from './MapComponent';
 
 class MainPage extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            directions: null,
+        }
+    }
+
+    test = (e) => {
+        e.preventDefault();
     }
 
     render() { 
         return ( 
             <div>
-                <button onClick={this.props.toggleDrawer}>Test</button>
+                <div onClick={this.props.toggleDrawer} className={"bottom-drawer"}> 
+                    <Button onClick={this.test} className={"go-button"} type="primary">GO</Button>  
+                </div>
+                <MapComponent
+                    googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVb99HgTAxKCABiclsF0X7uzoLCN3JnLQ&v=3.exp&libraries=geometry,drawing,places"
+                    loadingElement={<div style={{ height: `100%` }} />}
+                    containerElement={<div style={{ height: `100%` }} />}
+                    mapElement={<div style={{ height: `88.5vh` }} />}
+                />
                 <Drawer
                     open={this.props.drawerIsToggled}
                     onRequestClose={this.props.toggleDrawer}
---
+++ src/reducers/activitiesReducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+    activities: null,
+    current_activity: null
+}
+
+export const layoutReducer = (state = initialState, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
